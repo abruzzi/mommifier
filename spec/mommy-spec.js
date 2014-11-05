bc455454217dd2ce6f3c27208bf1227c90237d4c
Qiu Juntao <juntao.qiu@gmail.com>
2014-11-05 18:11:21 +1100

    add another test, and make it pass

--- spec/mommy-spec.js
+++ spec/mommy-spec.js
@@ -1,9 +1,14 @@
 describe("mommify", function() {
 	it("should not mommify if the vowels less than 30%", function() {
 		var expected = "hmm";
-
 		var result = mommify("hmm");
 
+		expect(result).toEqual(expected);
+	});
+
+	it("should mommify if the vowels greater than 30%", function() {
+		var expected = "shmommy";
+		var result = mommify("she");
 		expect(result).toEqual(expected);
 	});
 });
--- src/mommy.js
+++ src/mommy.js
@@ -1,3 +1,25 @@
-function mommify() {
-	return "hmm";
+function mommify(word) {
+	var count = 0;
+	for(var i = 0; i < word.length; i++) {
+		if(['a', 'e', 'i', 'o', 'u'].indexOf(word[i]) >= 0) {
+			count += 1;
+		}
+	}
+
+	var str = "";
+
+	if(count/word.length >= 0.30) {
+		
+		for(var i = 0; i < word.length; i++) {
+			if(['a', 'e', 'i', 'o', 'u'].indexOf(word[i]) >= 0) {
+				str += "mommy";
+			} else {
+				str += word[i];
+			}
+		}
+	} else {
+		str = word;
+	}
+
+	return str;
 }
