aba20a9e948a706d341b888310bedbf5ea1e6971
zhangsq <zhangsq@outlook.com>
2015-04-05 16:14:46 +0800

    使用正则表达式重构替换功能

--- src/mommy.js
+++ src/mommy.js
@@ -1,7 +1,8 @@
 var Mommifier = {
 	"VOWELS": "aeiou",
 	"REPLACEMENT": "mommy",
-	"THRESHOLD": 0.30
+	"THRESHOLD": 0.30,
+    "REPLACE_REG": /[aeiou]+/g
 }
 
 function isVowel(character) {
@@ -20,7 +21,7 @@ function countVowels(word) {
 	return count;
 }
 
-function next(current, previous) {
+/*function next(current, previous) {
 	var next = "";
 
 	if(isVowel(current)) {
@@ -32,16 +33,19 @@ function next(current, previous) {
 	}
 
 	return next;
-}
+}*/
 
 function replace(word) {
-	var str = "";
+	/*var str = "";
 
 	for(var i = 0; i < word.length; i++) {
 		str += next(word[i], word[i-1]);
 	}
 
-	return str;
+	return str;*/
+    
+    
+    return word.replace(Mommifier.REPLACE_REG, Mommifier.REPLACEMENT);
 }
 
 function shouldBeMommify(word) {
